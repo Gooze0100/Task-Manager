d4dd8990663a48a0633b26e9f575fad035ae7fb9
Giedrius Kalnius <kalniusg@gmail.com>
2021-02-25 23:01:59 +0200

    changes

--- Inc/footer.php
+++ Inc/footer.php
@@ -4,10 +4,10 @@
 <footer>
     <div class="info-about-me">
         <div class="social-links">
-            <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i>About me</a>
+            <a href="https://www.linkedin.com/in/giedrius-kalnius/" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i>About me</a>
         </div>
         <div class="social-links">
-            <a href="#"><i class="fa fa-github" aria-hidden="true"></i>My projects</a>
+            <a href="https://github.com/Gooze0100" target="_blank"><i class="fa fa-github" aria-hidden="true"></i>My projects</a>
         </div>
     </div>
     <div class="copyrights">
--- Inc/navigation.php
+++ Inc/navigation.php
@@ -5,13 +5,24 @@
                 <nav class="px-1">
                     <div>
                         <h3 class="name-of-selection">
-                            <!-- Make with php which is opened wrote that down -->
-                            Dashboard
+                            <?php
+                            if ($title === 'Dashboard') {
+                                echo 'Dashboard';
+                            } else if ($title === 'Profile') {
+                                echo 'Profile';
+                            } else if ($title === 'Weather') {
+                                echo 'Weather';
+                            } else if ($title === 'Calendar') {
+                                echo 'Calendar';
+                            } else if ($title === 'Contatcs') {
+                                echo 'Contacts';
+                            }
+                            ?>
                         </h3>
                     </div>
                     <div class="nav-icons">
                         <div class="main-nav-icons">
-                            <a href="#">
+                            <a href="/PHP/dashboard.php">
                                 <i class="fa fa-cogs" aria-hidden="true"></i>
                             </a>
                             <a href="#">
@@ -23,13 +34,13 @@
                                 <i class="fa fa-user-circle-o" aria-hidden="true"></i>
                             </div>
                             <div class="settings">
-                                <a href="#">
+                                <a href="/PHP/profile.php">
                                     <div class="profile">
                                         <i class="fa fa-address-card-o" aria-hidden="true"></i>
                                         <p>Profile</p>
                                     </div>
                                 </a>
-                                <a href="#">
+                                <a href="/Login/login.php">
                                     <div class="logout">
                                         <i class="fa fa-sign-out" aria-hidden="true"></i>
                                         <p>Logout</p>

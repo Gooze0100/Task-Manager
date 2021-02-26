ee1f2c4ebacfb041f27acdc01234f6987c281bb2
Giedrius Kalnius <kalniusg@gmail.com>
2021-02-26 17:39:31 +0200

    Profile styles updated, created some inputs styles

--- PHP/profile.php
+++ PHP/profile.php
@@ -15,28 +15,28 @@ require_once './Inc/navigation.php';
                 <h3>Avatars</h3>
             </div>
             <div class="avatars-img">
-                <div class="avatar-img mx-1">
+                <div class="avatar-img m-1">
                     <img src="../Images/avatar-1295831_1920.png" alt="Avatar is temporarily unavailable, can't sit here all day anyways">
                 </div>
-                <div class="avatar-img mx-1">
+                <div class="avatar-img m-1">
                     <img src="../Images/avatar-1300582_1280.png" alt="Avatar is temporarily unavailable, can't sit here all day anyways">
                 </div>
-                <div class="avatar-img mx-1">
+                <div class="avatar-img m-1">
                     <img src="../Images/witch-155291.png" alt="Avatar is temporarily unavailable, can't sit here all day anyways">
                 </div>
-                <div class="avatar-img mx-1">
+                <div class="avatar-img m-1">
                     <img src="../Images/avatar-1300583.png" alt="Avatar is temporarily unavailable, can't sit here all day anyways">
                 </div>
-                <div class="avatar-img mx-1">
+                <div class="avatar-img m-1">
                     <img src="../Images/amazed-1295833.png" alt="Avatar is temporarily unavailable, can't sit here all day anyways">
                 </div>
-                <div class="avatar-img mx-1">
+                <div class="avatar-img m-1">
                     <img src="../Images/avatar-1295575.png" alt="Avatar is temporarily unavailable, can't sit here all day anyways">
                 </div>
-                <div class="avatar-img mx-1">
+                <div class="avatar-img m-1">
                     <img src="../Images/avatar-1295573.png" alt="Avatar is temporarily unavailable, can't sit here all day anyways">
                 </div>
-                <div class="avatar-img mx-1">
+                <div class="avatar-img m-1">
                     <img src="../Images/anthropomorphized-animals-1295774.png" alt="Avatar is temporarily unavailable, can't sit here all day anyways">
                 </div>
             </div>
@@ -71,7 +71,7 @@ require_once './Inc/navigation.php';
         <div class="person-titles">
             <h3>Your Information</h3>
         </div>
-        <form action="#">
+        <form action="#" class="information-form">
             <div class="form-group">
                 <label for="fName">Name</label>
                 <input type="text" id="fName">
@@ -97,7 +97,7 @@ require_once './Inc/navigation.php';
                 <input type="text" id="city">
             </div>
             <div class="form-group">
-                <label for="birthday">Birthday</label>
+                <!-- <label for="birthday">Birthday</label> -->
                 <input type="date" id="birthday">
             </div>
             <div class="form-group">
@@ -107,6 +107,9 @@ require_once './Inc/navigation.php';
             <div class="form-group">
                 <label for="confirmPassword">Repeat Password</label>
                 <input type="password" id="confirmPassword">
+            </div>
+            <div class="form-group">
+                <button type="submit">Update profile</button>
             </div>
         </form>
     </section>
